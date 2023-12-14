d115b1a49b8696e547dc045b759fe70dc7e63a60
Otávio Negrizolli <otavionegrizolli@hotmail.com>
2023-12-14 12:40:35 -0300

    add paths html

--- src/frontend/frontend.routes.js
+++ src/frontend/frontend.routes.js
@@ -16,23 +16,30 @@ frontendRoutes.get('^/$|/index(\.html)?', (_, res) => {
 
 // drivers
 frontendRoutes.get('^/motoristas/:id', (_, res) => {
-    return res.sendFile(path.join(__dirname, 'restricted', 'driver.html'));
+    return res.sendFile(path.join(__dirname, 'restricted', 'driver', 'driver.html'));
 }).get('^/motoristas', (_, res) => {
-    return res.sendFile(path.join(__dirname, 'restricted', 'driver-list.html'));
+    return res.sendFile(path.join(__dirname, 'restricted', 'driver', 'index.html'));
 });
 
 // vehicles
 frontendRoutes.get('^/veiculos', (_, res) => {
-    return res.sendFile(path.join(__dirname, 'restricted', 'vehicle-list.html'));
+    return res.sendFile(path.join(__dirname, 'restricted', 'vehicle', 'index.html'));
 }).get('^/veiculos/:id', (_, res) => {
-    return res.sendFile(path.join(__dirname, 'restricted', 'vehicle.html'));
+    return res.sendFile(path.join(__dirname, 'restricted', 'vehicle', 'vehicle.html'));
+});
+
+// path
+frontendRoutes.get('^/trajetos', (_, res) => {
+    return res.sendFile(path.join(__dirname, 'restricted', 'path', 'index.html'));
+}).get('^/trajetos/:id', (_, res) => {
+    return res.sendFile(path.join(__dirname, 'restricted', 'path', 'path.html'));
 });
 
 // assets
 frontendRoutes.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // fallback
-frontendRoutes.get('*', (req,res) => {
+frontendRoutes.get('*', (req, res) => {
     res.status(404);
     return res.sendFile(path.join(__dirname, 'public', '404.html'))
 });
